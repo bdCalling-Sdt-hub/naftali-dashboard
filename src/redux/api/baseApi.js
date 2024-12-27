99bd79c9fcfd04a9f677948aa6e5cf6f1e1f89f8
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2024-12-27 18:53:01 +0600

    updated

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import toast from "react-hot-toast";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://192.168.10.15:5000/api/v1",
+  baseUrl: "http://139.59.0.25:5001/api/v1",
   prepareHeaders: (headers) => {
     const token =
       localStorage.getItem("authToken") || sessionStorage.getItem("authToken");
@@ -45,4 +45,4 @@ export const api = createApi({
 });
 
 // Export the image URL as a constant
-export const imageUrl = "http://206.189.231.81:5000";
+export const imageUrl = "http://139.59.0.25:5001";
